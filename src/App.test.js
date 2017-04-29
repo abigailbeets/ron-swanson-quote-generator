0671fbca637f8ce895b649aa66dab0b64b54b2fb
Abigail Beets <abigail.beets@gmail.com>
2017-04-29 07:53:38 -0400

    add ron swanson photo div with render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
 
 describe("When the Ron Swanson Quote Generator page loads", () => {
-  it('renders a picture of Ron Swanson', () => {
+  it('renders the Ron Swanson photo div', () => {
     const div = document.createElement('div')
-    const wrapper = ReactDOM.render(<App />, div)
+    const wrapper = shallow(<App />, div)
     expect(wrapper.find('#ron-swanson-photo')).to.have.length(1)
   })
-});
+})
