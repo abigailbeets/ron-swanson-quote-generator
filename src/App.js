2e7b178be55b9e954131fab20eb411a3a72e530c
Abigail <abigail.beets@gmail.com>
2017-05-25 17:15:12 -0400

    Reorganize test and css files Add test Ron Swanson photo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../src/css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div id="ron-swanson-photo">
-        <div className="App-header">
-        </div>
+        <img src='http://swansonquotes.com/wp-content/uploads/s07-ep04-startfire4-1000x500.jpg' alt='ron-swanson' />
       </div>
     );
   }
